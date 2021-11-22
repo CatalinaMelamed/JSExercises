2fbbda26e3b8c11f014839db6ad247b89a5d8566
CatalinaMelamed <catalinamelamed189@hotmail.com>
2021-11-22 20:12:08 -0300

    Finished AddOrder Button

--- ProcessBar/function.js
+++ ProcessBar/function.js
@@ -3,14 +3,21 @@ function addOrder(valor, colorBarra){
   var currentBar = document.getElementById(currentid);
   var widthActual = currentBar.style.width.substring(currentBar.style.width.length -1, 0); //cortar string
   if (widthActual == ""){widthActual = "0"} //validacion para el primer click
-  
+ 
   currentBar.style.width = (parseInt(widthActual) + 10) + '%'; //parciar string y sumarlo
   currentBar.style.backgroundColor = colorBarra;
   currentid ='demo{0}'.replace('{0}', valor);
-  var currentSpan = document.getElementById(currentid);
-  
-  currentid ='message{0}'.replace('{0}', valor);
-  var currentp = document.getElementById(currentid);
+  var currentSpan = document.getElementById(currentid);  
+  currentSpan.innerHTML = (parseInt(widthActual) + 10) / 10;
+  if (currentBar.style.width == '100%')
+  {
+    currentid = 'btnAdd_{0}'.replace('{0}', valor);
+    var currentBtn = document.getElementById(currentid);
+    currentBtn.disabled = true;
+    currentid ='messagebar{0}'.replace('{0}', valor);
+    var currentP = document.getElementById(currentid);
+    currentP.style.display = 'inline';
+  }
 }
 
 // currentBar-style.width = (currentBar.style.clientwidth + 10) + '%';
