a0ebce3f534cf1a0ede42bcec07d87a2cbdfc516
CatalinaMelamed <catalinamelamed189@hotmail.com>
2021-11-23 23:22:42 -0300

    Final Version 1

--- ProcessBar/function.js
+++ ProcessBar/function.js
@@ -3,6 +3,13 @@ function addOrder(valor, colorBarra){
   var currentBar = document.getElementById(currentid);
   var widthActual = currentBar.style.width.substring(currentBar.style.width.length -1, 0); //cortar string
   if (widthActual == ""){widthActual = "0"} //validacion para el primer click
+
+  if (currentBar.style.width == '0%')
+  {
+    currentid = 'btnSub_{0}'.replace('{0}', valor);
+    var currentBtn = document.getElementById(currentid);
+    currentBtn.disabled = false;
+  } 
  
   currentBar.style.width = (parseInt(widthActual) + 10) + '%'; //parciar string y sumarlo
   currentBar.style.backgroundColor = colorBarra;
@@ -17,6 +24,36 @@ function addOrder(valor, colorBarra){
     currentid ='messagebar{0}'.replace('{0}', valor);
     var currentP = document.getElementById(currentid);
     currentP.style.display = 'inline';
+  }
+}
+
+function subtractOrder (valor, colorBarra){  
+  var currentid ='bar_{0}'.replace('{0}', valor);
+  var currentBar = document.getElementById(currentid);
+  var widthActual = currentBar.style.width.substring(currentBar.style.width.length -1, 0); //cortar string
+  if (widthActual == ""){widthActual = "0"} //validacion para el primer click  
+
+  if (currentBar.style.width == '100%')
+  {
+    currentid = 'btnAdd_{0}'.replace('{0}', valor);
+    var currentBtn = document.getElementById(currentid);
+    currentBtn.disabled = false;
+  } 
+  currentBar.style.width = (parseInt(widthActual) - 10) + '%'; //parciar string y sumarlo
+  currentBar.style.backgroundColor = colorBarra;
+  currentid ='demo{0}'.replace('{0}', valor);
+  var currentSpan = document.getElementById(currentid);  
+  currentSpan.innerHTML = (parseInt(widthActual) - 10) / 10;
+
+  if (currentBar.style.width == '0%')
+  {
+    currentid = 'btnSub_{0}'.replace('{0}', valor);
+    var currentBtn = document.getElementById(currentid);
+    currentBtn.disabled = true;
+    currentid ='messagebar{0}'.replace('{0}', valor);
+    var currentP = document.getElementById(currentid);
+    currentP.style.display = 'inline';
+    
   }
 }
 
